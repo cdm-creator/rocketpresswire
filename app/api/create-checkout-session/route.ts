a65da51ff81f3dcd1235fb5eab439412ee578797
cdm-creator <agencyanalytics.cdm@gmail.com>
2026-07-06 14:58:07 +0530

    created

--- app/api/create-checkout-session/route.ts
+++ app/api/create-checkout-session/route.ts
@@ -8,6 +8,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string)
 const PRODUCT_PRICE_MAP: Record<string, string> = {
     msn: "price_1Tq8bFRvo61AD2cgV6by04aS",
     reuters: "price_1Tq8cPRvo61AD2cgeWCTcRyd",
+    openPR:"price_1Tq8csRvo61AD2cgaOaDm646",
 }
 
 const corsHeaders = {
