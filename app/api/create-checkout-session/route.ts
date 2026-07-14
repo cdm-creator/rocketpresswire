b1ea7bf17c0ed94a5c07beb131cd2b6f8ed8b62e
cdm-creator <agencyanalytics.cdm@gmail.com>
2026-07-14 10:05:50 +0530

    In single distribution page new products added

--- app/api/create-checkout-session/route.ts
+++ app/api/create-checkout-session/route.ts
@@ -9,6 +9,13 @@ const PRODUCT_PRICE_MAP: Record<string, string> = {
     msn: "price_1Tq8bFRvo61AD2cgV6by04aS",
     reuters: "price_1Tq8cPRvo61AD2cgeWCTcRyd",
     openPR:"price_1Tq8csRvo61AD2cgaOaDm646",
+    core: "price_1TsxfzRvo61AD2cgCeCRxiJB",
+    growth: "price_1TsxgZRvo61AD2cg4Q2t2Yv8",
+    premium: "price_1Tsxh2Rvo61AD2cgU3FfoNYE",
+    enterprise: "price_1TsxheRvo61AD2cgfOqfW44W",
+    morningstar: "price_1TsxrCRvo61AD2cgEWqMfI66",
+    apple_news: "price_1TsxxmRvo61AD2cg2TMuv2yl",
+    big_news_network: "price_1TsxvFRvo61AD2cgSqKisJd6",
 }
 
 const corsHeaders = {
